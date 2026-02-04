ec426deff6e930bfed4722cecaddea85e0d20a12
AmanKumar9122 <amanraj15112003@gmail.com>
2026-02-04 21:15:13 +0530

    4/2/26

---
+++ Q11111111.java
@@ -0,0 +1,8 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Q11111111 {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in))
+    }
+}
