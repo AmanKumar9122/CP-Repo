1b8eb4d7209b39cc8c8bf381502a43feea2487a8
AmanKumar9122 <amanraj15112003@gmail.com>
2026-02-17 14:28:49 +0530

    Q427A

---
+++ Q427A.java
@@ -0,0 +1,22 @@
+public class Q427A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int availablePolice = 0;
+        int untreatedCrimes = 0;
+        for (int i = 0; i < n; i++) {
+            int event = sc.nextInt();
+            if (event == -1) {
+                // Crime occurred
+                if (availablePolice > 0) {
+                    availablePolice--;
+                } else {
+                    untreatedCrimes++;
+                }
+            } else {
+                availablePolice += event;
+            }
+        }
+        System.out.println(untreatedCrimes);
+    }
+}
